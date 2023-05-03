5ac84753594361fdcf315a4119164e03039ba498
Charles Lee <ckl@google.com>
2023-05-03 08:32:22 -0700

    Add Python macOS continuous/presubmit jobs to verify release artifact creation.
    PiperOrigin-RevId: 529098938
    Change-Id: I18996c044d8b300a08724491e2de129e32c1b361

---
+++ kokoro/macos_external/release/run_tests.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+
+# The user may specify TINK_BASE_DIR for setting a local copy of Tink to use
+# when running the script locally.
+
+set -euo pipefail
+
+# If we are running on Kokoro cd into the repository.
+if [[ -n "${KOKORO_ROOT:-}" ]]; then
+  TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
+  cd "${TINK_BASE_DIR}/tink_py"
+fi
+
+: "${TINK_BASE_DIR:=$(cd .. && pwd)}"
+
+# Check for dependencies in TINK_BASE_DIR. Any that aren't present will be
+# downloaded.
+readonly GITHUB_ORG="https://github.com/tink-crypto"
+./kokoro/testutils/fetch_git_repo_if_not_present.sh "${TINK_BASE_DIR}" \
+  "${GITHUB_ORG}/tink-cc"
+
+./kokoro/testutils/copy_credentials.sh "testdata" "all"
+
+# Generate and test release artifacts.
+./tools/distribution/create_release.sh
