d63988b55f9a109ebafc8f382bfca8df1408ffce
Moreno Ambrosin <ambrosin@google.com>
2023-07-21 01:25:30 -0700

    Add missing calls to kokoro/testutils/copy_credentials.sh
    PiperOrigin-RevId: 549872877
    Change-Id: I31cd33de13b4fa1e322d51316119c187d522d61f

--- kokoro/macos_external/release/bdist/create/run.sh
+++ kokoro/macos_external/release/bdist/create/run.sh
@@ -36,6 +36,8 @@ readonly GITHUB_ORG="https://github.com/tink-crypto"
 ./kokoro/testutils/fetch_git_repo_if_not_present.sh "${TINK_BASE_DIR}" \
   "${GITHUB_ORG}/tink-cc"
 
+./kokoro/testutils/copy_credentials.sh "testdata" "all"
+
 CREATE_DIST_OPTIONS=()
 if [[ "${IS_KOKORO}" == "true" ]]; then
   if [[ "${KOKORO_PARENT_JOB_NAME:-}" =~ tink/github/py/.*_release ]]; then
