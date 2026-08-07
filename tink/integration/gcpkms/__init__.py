0b6db76d8656288da564bc4214c22e750348d540
Md Rafi Ur Rashid <rafiurrashid@google.com>
2026-08-07 08:41:14 -0700

    Expose GCP KMS MAC and digital signature functions in the Tink Python GCP KMS package.
    PiperOrigin-RevId: 960948613
    Change-Id: I302403b6be3bec301843ebb55e1cb44731241f3c

--- tink/integration/gcpkms/__init__.py
+++ tink/integration/gcpkms/__init__.py
@@ -17,6 +17,9 @@
 try:
   # pylint: disable=g-import-not-at-top
   from tink.integration.gcpkms import _gcp_kms_client
+  from tink.integration.gcpkms import _gcp_kms_mac
+  from tink.integration.gcpkms import _gcp_kms_public_key_sign
+  from tink.integration.gcpkms import _gcp_kms_public_key_verify
 except ImportError as import_error:
   raise ImportError(
       'Error importing the Tink Google Cloud KMS module; did you forget to'
@@ -25,3 +28,13 @@ except ImportError as import_error:
 
 GcpKmsClient = _gcp_kms_client.GcpKmsClient
 new_client = _gcp_kms_client.new_client
+new_gcp_kms_mac = _gcp_kms_mac.new_gcp_kms_mac
+new_gcp_kms_public_key_sign = (
+    _gcp_kms_public_key_sign.new_gcp_kms_public_key_sign
+)
+new_gcp_kms_public_key_verify = (
+    _gcp_kms_public_key_verify.new_gcp_kms_public_key_verify
+)
+new_gcp_kms_public_key_verify_no_rpc = (
+    _gcp_kms_public_key_verify.new_gcp_kms_public_key_verify_no_rpc
+)
